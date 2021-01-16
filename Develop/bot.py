88cca4ab5435dbd67f94613d810c309b1edbbde0
Vincent Lafrance <vincent.lafrance.2@ulaval.ca>
2021-01-15 22:13:16 -0500

    test

--- Develop/bot.py
+++ Develop/bot.py
@@ -18,15 +18,15 @@ class Bot:
         MAX_MINER_AMOUNT = 4
         MAX_OUTLAW_AMOUNT = 1
 
-        actions: List[Action] = [self.get_miner_action(unit, game_map, crews, my_crew, my_id, rules) for unit in my_crew.units]
+        actions: List[Action] = [self.get_miner_action(unit, game_map, crews, my_crew, my_id, rules) for unit in my_crew.units if unit.type == UnitType.MINER]
 
-        
+        actions.append(self.)
 
         if(my_crew.blitzium >= my_crew.prices.MINER and len(list(filter(lambda unit: unit.type == UnitType.MINER, my_crew.units))) < MAX_MINER_AMOUNT) :
             actions.append(BuyAction(UnitType.MINER))
 
-        #if(my_crew.blitzium >= 250 and len(list(filter(lambda unit: unit.type == UnitType.MINER, my_crew.units))) < MAX_OUTLAW_AMOUNT) :
-        #    actions.append(BuyAction(UnitType.OUTLAW))
+        if(my_crew.blitzium >= 250 and len(list(filter(lambda unit: unit.type == UnitType.MINER, my_crew.units))) < MAX_OUTLAW_AMOUNT) :
+            actions.append(BuyAction(UnitType.OUTLAW))
 
         return actions
 
@@ -91,7 +91,7 @@ class Bot:
         target = minable if minable is not None else self.get_random_position(map.get_map_size())
         return UnitAction(UnitActionType.MOVE, unit.id, target)
 
-    #def get_outlaw_action(self, unit, map, crews, my_crews, my_id) :
+    def get_outlaw_action(self, unit, map, crews, my_crews, my_id) :
         
 
     def drop_home(self, unit, my_crew, map):
